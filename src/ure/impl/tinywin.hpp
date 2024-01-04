d8783407657d79cddf2dad2001537ac4353c7311
MateuszJanduraUszu <Mati4321@interia.pl>
2024-01-04 17:42:30 +0100

    Add `<main.cpp>` and `<tinywin.hpp>`

---
+++ src/ure/impl/tinywin.hpp
@@ -0,0 +1,13 @@
+// tinywin.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _URE_IMPL_TINYWIN_HPP_
+#define _URE_IMPL_TINYWIN_HPP_
+
+#define WIN32_LEAN_AND_MEAN
+#define NOSERVICE
+#include <Windows.h>
+#endif // _URE_IMPL_TINYWIN_HPP_
